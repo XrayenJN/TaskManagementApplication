1aeb82fc3ca7320da8aa032acb84a53dc16fddce
Jordan Nathanael <jordannathanael0@gmail.com>
2024-06-26 17:40:03 +1000

    Fix: Transform into function-based component with hooks

--- TaskManagementSystem/src/Home.jsx
+++ TaskManagementSystem/src/Home.jsx
@@ -1,53 +1,54 @@
 import React, { Component } from 'react';
-import { withOktaAuth } from '@okta/okta-react';
+import { useOktaAuth, withOktaAuth } from '@okta/okta-react';
+import { useHistory } from 'react-router-dom'
 import './App.css';
 import reactLogo from './assets/react.svg'
 
-export default withOktaAuth(class Home extends Component {
-  constructor(props) {
-    super(props);
-    this.login = this.login.bind(this);
-    this.logout = this.logout.bind(this);
+export default function Home(){
+  const { authState, oktaAuth } = useOktaAuth();
+  const history = useHistory();
+
+  const login = async () => {
+    await oktaAuth.signInWithRedirect();
   }
 
-  async login() {
-    await this.props.oktaAuth.signInWithRedirect();
+  const logout = async () => {
+    await oktaAuth.signOut();
   }
 
-  async logout() {
-    await this.props.oktaAuth.signOut();
+  const goToProfile = () => {
+    history.push('/profile');
   }
 
-  render() {
-    let body = null;
-    if (this.props.authState?.isAuthenticated) {
+  let body = null;
+    if (authState?.isAuthenticated) {
       body = (
         <div className="Buttons">
-          <button onClick={this.logout}>Logout</button>
+          <button onClick={goToProfile}>Profile</button>
+          <button onClick={logout}>Logout</button>
           {/* Replace me with your root component. */}
         </div>
       );
     } else {
       body = (
         <div className="Buttons">
-          <button onClick={this.login}>Login</button>
+          <button onClick={login}>Login</button>
         </div>
       );
     }
 
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={reactLogo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/Home.js</code> and save to reload.
-          </p>
-          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-            Learn React
-          </a>
-          {body}
-        </header>
-      </div>
-    );
-  }
-});
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={reactLogo} className="App-logo" alt="logo"/>
+        <p>
+          Edit <code>src/Home.js</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+        {body}
+      </header>
+    </div>
+  );
+}
