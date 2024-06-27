6c03409a847a500f673b16c22e98970c2d9ee8ba
Jordan Nathanael <jordannathanael0@gmail.com>
2024-06-27 12:59:21 +1000

    Fix: Redo the firebase authentication because the previous project doesn't really work in the authentication part

--- TaskManagementSystem/server/firebase/firebase.js
+++ TaskManagementSystem/server/firebase/firebase.js
@@ -1,10 +1,51 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
-import 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import firebaseConfig from './firebcraseConfig.js';
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKv_xoVeTNAQOIgJAL0RSA8aob0d3J5A8",
+  authDomain: "task-management-system-cs-01.firebaseapp.com",
+  projectId: "task-management-system-cs-01",
+  storageBucket: "task-management-system-cs-01.appspot.com",
+  messagingSenderId: "438150743957",
+  appId: "1:438150743957:web:ded891ce2568435001989c"
+};
 
-firebase.initializeApp(firebaseConfig);
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+export const googleSignIn = async () => {
+    await signInWithPopup(auth, provider)
+    .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+        console.log(user)
+        console.log(token)
+        if (window.opener){
+          window.opener.postMessage({ user: result.user }, "*");
+          window.close();
+        }
+      }).catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+        console.log(errorCode);
+        console.log(errorMessage);
+      });
+    ;
+}
--- TaskManagementSystem/server/index.js
+++ TaskManagementSystem/server/index.js
@@ -8,6 +8,13 @@ app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
+// Set COOP header for all responses
+app.use((req, res, next) => {
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
+  next();
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
