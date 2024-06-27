a8022d80f2c715ad9b2a6f7818bc24ceb8b70326
Jordan Nathanael <jordannathanael0@gmail.com>
2024-06-27 12:58:13 +1000

    Fix: Minimize the output from the profile, try to get the email only

--- TaskManagementSystem/client/src/Profile.jsx
+++ TaskManagementSystem/client/src/Profile.jsx
@@ -1,31 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { useOktaAuth } from '@okta/okta-react';
 import { Header, Icon, Table } from 'semantic-ui-react';
 import { useHistory } from 'react-router-dom'
+import { AuthContext } from './AuthContext';
 
 const Profile = () => {
-  const { authState, oktaAuth } = useOktaAuth();
-  const [userInfo, setUserInfo] = useState(null);
+  const { user, oktaAuth, auth } = useContext(AuthContext); 
   const history = useHistory();
+
+  const handleLogout = async() => {
+    await oktaAuth.signOut();
+    await auth.signOut();;
+  }
 
   const goBack = () => {
     history.goBack();
   };
 
-  useEffect(() => {
-    if (!authState || !authState.isAuthenticated) {
-      // When user isn't authenticated, forget any user info
-      setUserInfo(null);
-    } else {
-      setUserInfo(authState.idToken.claims);
-      // You can also get user information from the `/userinfo` endpoint
-      /*oktaAuth.getUser().then((info) => {
-        setUserInfo(info);
-      });*/
-    }
-  }, [authState, oktaAuth]); // Update if authState changes
-
-  if (!userInfo) {
+  if (!user) {
     return (
       <div>
         <p>Fetching user profile...</p>
@@ -37,48 +29,11 @@ const Profile = () => {
   return (
     <div>
       <div>
-        <Header as="h1">
-          <Icon name="drivers license" />
-          {' '}
-          My User Profile (ID Token Claims)
-          {' '}
-        </Header>
-        <p>
-          Below is the information from your ID token which was obtained during the &nbsp;
-          <a href="https://developer.okta.com/docs/guides/implement-auth-code-pkce">PKCE Flow</a>
-          {' '}
-          and is now stored in local storage.
-        </p>
-        <p>
-          This route is protected with the
-          {' '}
-          <code>&lt;SecureRoute&gt;</code>
-          {' '}
-          component, which will ensure that this page cannot be accessed until you have authenticated.
-        </p>
-        <Table>
-          <thead>
-            <tr>
-              <th>Claim</th>
-              <th>Value</th>
-            </tr>
-          </thead>
-          <tbody>
-            {Object.entries(userInfo).map((claimEntry) => {
-              const claimName = claimEntry[0];
-              const claimValue = claimEntry[1];
-              const claimId = `claim-${claimName}`;
-              return (
-                <tr key={claimName}>
-                  <td>{claimName}</td>
-                  <td id={claimId}>{claimValue.toString()}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      </div>
-      <button onClick={goBack}>Back</button>
+      <h1>User Profile</h1>
+      <p><strong>Name:</strong> {user.name || user.displayName}</p>
+      <p><strong>Email:</strong> {user.email}</p>
+    </div>
+      <button onClick={handleLogout}>Log Out</button>
     </div>
   );
 };
