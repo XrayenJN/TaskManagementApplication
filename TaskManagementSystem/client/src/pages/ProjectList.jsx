13c0d81d99dac5ac9a83b4f18ba1652a3f0e1472
John Bui <jbui0004@student.monash.edu>
2024-08-06 02:18:23 +1000

    Add project start date, fix formatting

--- TaskManagementSystem/client/src/pages/ProjectList.jsx
+++ TaskManagementSystem/client/src/pages/ProjectList.jsx
@@ -18,7 +18,6 @@ const ProjectList = () => {
 
   const fetchContributors = async (projectId) => {
     const theContributors = await getContributors(projectId);
-    //setContributors(prevState => ({...prevState, [projectId]: theContributors}));
     setContributors(theContributors);
   };
 
@@ -133,8 +132,9 @@ const ProjectList = () => {
                   </div>
                 </div>
                 <div>
-                  <div style={{ fontWeight: 'bold', color: 'black', display: 'flex', alignItems: 'center' , fontSize: '18px' }}>
-                    End date: {project.endDate}
+                  <div style={{ color: 'black', fontSize: '18px' }}>
+                    <div><b>Start date:</b> {project.startDate}</div>
+                    <div><b>End date:</b> {project.endDate}</div>
                   </div>
                   <div style={{margin: '15px 0', textAlign: 'right'}}>
                     <Link to="/edit-project" style={{ backgroundColor: '#DEB992', color: 'black', padding: '5px 10px', border: 'none', cursor: 'pointer' }}>
@@ -158,8 +158,9 @@ const ProjectList = () => {
                   </div>
                 </div>
                 <div>
-                  <div style={{ fontWeight: 'bold', color: 'black', display: 'flex', alignItems: 'center' , fontSize: '18px' }}>
-                    End date: {project.endDate}
+                  <div style={{ color: 'black', fontSize: '18px' }}>
+                    <div><b>Start date:</b> {project.startDate}</div>
+                    <div><b>End date:</b> {project.endDate}</div>
                   </div>
                   <div style={{margin: '15px 0', textAlign: 'right'}}>
                     <Link to="/edit-project" style={{ backgroundColor: '#DEB992', color: 'black', padding: '5px 10px', border: 'none', cursor: 'pointer' }}>
