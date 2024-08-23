3f0fdcf268d6514a8ee997152f4febf88e5000ab
John Bui <jbui0004@student.monash.edu>
2024-08-23 22:06:16 +1000

    Add AuthProvider to main root and TitleBar

--- TaskManagementSystem/client/src/components/TitleBar.jsx
+++ TaskManagementSystem/client/src/components/TitleBar.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AuthContext } from '../contexts/AuthContext';
 
 const TitleBar = ({ toggleNavbar }) => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div style={{ backgroundColor: '#051622', color: '#fff', display: 'flex', alignItems: 'center', padding: '10px 20px', position: 'fixed', width: '100%', top: 0, left: 0, zIndex: 1 }}>
       <h1 style={{ margin: 0, textAlign: 'center', flex: 1 }}>Task Management System</h1>
--- TaskManagementSystem/client/src/main.jsx
+++ TaskManagementSystem/client/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './assets/styles/index.css'
 import RouterApp from './App.jsx'
-// import RouterApp from './App.jsx'
+import { AuthProvider } from './contexts/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterApp />
+    <AuthProvider>
+      <RouterApp />
+    </AuthProvider>
   </React.StrictMode>,
 )
