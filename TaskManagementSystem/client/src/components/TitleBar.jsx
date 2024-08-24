f672cbd42652d43f3fd367a259c13a3e71fccf43
John Bui <jbui0004@student.monash.edu>
2024-08-24 17:33:32 +1000

    Add title bar profile icon and logged in user name

--- TaskManagementSystem/client/src/components/TitleBar.jsx
+++ TaskManagementSystem/client/src/components/TitleBar.jsx
@@ -1,5 +1,7 @@
 import React, { useContext } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const TitleBar = ({ toggleNavbar }) => {
   const { user } = useContext(AuthContext);
@@ -7,6 +9,15 @@ const TitleBar = ({ toggleNavbar }) => {
   return (
     <div style={{ backgroundColor: '#051622', color: '#fff', display: 'flex', alignItems: 'center', padding: '10px 20px', position: 'fixed', width: '100%', top: 0, left: 0, zIndex: 1 }}>
       <h1 style={{ margin: 0, textAlign: 'center', flex: 1 }}>Task Management System</h1>
+      {user && (
+        <div style={{ display: 'flex', alignItems: 'center', marginRight: '50px' }}>
+          <FontAwesomeIcon
+            icon={faUser}
+            style={{ fontSize: '30px', marginLeft: '10px' }}
+          />
+          <span style={{ marginLeft: '10px' }}>{user.displayName}</span>
+        </div>
+      )}
     </div>
   );
 };
