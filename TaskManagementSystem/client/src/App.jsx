ae91eeb2d7e22478aef28cf7753db95149e18113
John Bui <jbui0004@student.monash.edu>
2024-08-26 01:09:29 +1000

    Add Settings page

--- TaskManagementSystem/client/src/App.jsx
+++ TaskManagementSystem/client/src/App.jsx
@@ -10,6 +10,7 @@ import ProjectList from './pages/HomePage/HomeView';
 import Login from './pages/Login/Login';
 import Profile from './pages/Debugging/Profile';
 import PrivateRoute from './routes/PrivateRoute';
+import Settings from './pages/Settings';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/" exact={true} component={Login}/>
           <Route path="/login/callback" component={LoginCallback}/>
           <PrivateRoute path="/profile" component={Profile}/>
+          <Route path="/settings" component={Settings} />
           <PrivateRoute path="/new-project-form" component={NewProjectForm}/>
           <PrivateRoute path="/projects" component={ProjectList} />
           <PrivateRoute path="/project/:projectId" exact={true} component={ListView} />
