ff2b4e1ece5a63ef73b7dc926493634848fdab35
John Bui <jbui0004@student.monash.edu>
2024-08-28 15:57:28 +1000

    Add user profile details and back button

--- TaskManagementSystem/client/src/pages/Debugging/Profile.jsx
+++ TaskManagementSystem/client/src/pages/Debugging/Profile.jsx
@@ -6,6 +6,12 @@ const Profile = () => {
   const { user, oktaAuth, auth } = useContext(AuthContext); 
   const history = useHistory();
 
+  console.log(user.providerData[0].providerId);
+  console.log(user.providerData[0].photoURL);
+
+  console.log(user.metadata.creationTime);
+  console.log(user.metadata.lastSignInTime);
+
   const handleLogout = async() => {
     await oktaAuth.signOut();
     await auth.signOut();;
@@ -16,6 +22,7 @@ const Profile = () => {
   };
 
   let body = null;
+
   if (!user) {
     body = (
       <div>
@@ -24,11 +31,27 @@ const Profile = () => {
       </div>
     );
   } else {
+    const providerId = user.providerData ? user.providerData[0].providerId : null;
+
     body = (
       <div>
         <h1>User Profile</h1>
         <p><strong>Name:</strong> {user.name || user.displayName}</p>
         <p><strong>Email:</strong> {user.email}</p>
+
+        {providerId === 'google.com' && (
+          <>
+            <img src={user.providerData[0].photoURL} alt="Profile Picture" />
+            <br></br><br></br>
+            <p><strong>Account Provider:</strong> Google</p>
+            <p><strong>Account Created:</strong> {user.metadata.creationTime}</p>
+            <p><strong>Last Sign-In:</strong> {user.metadata.lastSignInTime}</p>
+          </>
+        )}
+
+        {providerId === null && (
+          <p><strong>Account Provider:</strong> Okta</p>
+        )}
       </div>
     )
   }
@@ -36,6 +59,7 @@ const Profile = () => {
   return (
     <div>
       {body}
+      <button onClick={goBack}>Back</button>
       <button onClick={handleLogout}>Log Out</button>
     </div>
   );
