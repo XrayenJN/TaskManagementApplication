d06579c3eba738326062a0646051a5316910fbf2
John Bui <jbui0004@student.monash.edu>
2024-08-29 13:39:23 +1000

    Add Kanban project view page to site

--- TaskManagementSystem/client/src/App.jsx
+++ TaskManagementSystem/client/src/App.jsx
@@ -4,6 +4,7 @@ import { Route, BrowserRouter as Router, Switch, withRouter } from 'react-router
 import TitleBar from './components/TitleBar';
 import { AuthProvider } from './contexts/AuthContext';
 import ListView from './pages/ListView';
+import KanbanView from './pages/HomePage/TaskViews/KanBanBoardView/KanBanBoard';
 import NewProjectTaskForm from './pages/NewProjectTaskForm';
 import NewProjectForm from './pages/HomePage/NewProjectForm';
 import ProjectList from './pages/HomePage/HomeView';
@@ -24,7 +25,8 @@ function App() {
           <Route path="/settings" component={Settings} />
           <PrivateRoute path="/new-project-form" component={NewProjectForm}/>
           <PrivateRoute path="/projects" component={ProjectList} />
-          <PrivateRoute path="/project/:projectId" exact={true} component={ListView} />
+          <Route path="/project/:projectId" exact={true} component={ListView} />
+          <Route path="/project/:projectId/kanban" component={KanbanView} />
           <PrivateRoute path="/project/:projectId/new-project-task-form" component={NewProjectTaskForm} />
         </AuthProvider>
       </Switch>
