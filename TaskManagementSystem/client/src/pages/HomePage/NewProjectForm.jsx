addd9d5041a32e61aefe6b0423598e76e818996c
Edward <wodainimenda1@gmail.com>
2024-09-11 22:07:08 +1000

    Update: Made the button aligned with input box

--- TaskManagementSystem/client/src/pages/HomePage/NewProjectForm.jsx
+++ TaskManagementSystem/client/src/pages/HomePage/NewProjectForm.jsx
@@ -8,11 +8,11 @@ const NewProjectForm = () => {
   const [description, setDescription] = useState('No Description Given');
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
+  const [contributors, setContributor] = useState([]);
   const history = useHistory();
 
   useEffect(() => {
     // Disable scrolling
-    document.body.style.overflow = 'hidden';
     document.body.style.margin = '0'; // Remove any default margin
     document.body.style.backgroundColor = '#F4F1E7';
 
@@ -36,12 +36,12 @@ const NewProjectForm = () => {
 
   const handleCancel = () => {
     history.goBack();
-  } 
+  }
 
   return (
     <div style={{ minHeight: '100vh', width: '100vw', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <div style={{ backgroundColor: '#DEB992', width: '50%', boxSizing: 'border-box'}}>
-        <h1 style={{ marginTop: '100px' }}>Create New Project</h1>
+        <h1 style={{ marginTop: '50px' }}>Create New Project</h1>
         <form onSubmit={handleSubmit}>
           <div>
             <h2>Project Name</h2>
@@ -53,14 +53,14 @@ const NewProjectForm = () => {
               required
             />  
           </div>
-          <div style={{paddingTop:'10px'}}>
+          <div style={{paddingTop: '10px'}}>
             <textarea
               placeholder='Description'
               onChange={(e) => setDescription(e.target.value)}
               required
             />
           </div>
-          <div>
+          <div style={{paddingTop: '10px'}}>
             <h2>Start Date:</h2>
             <input
               type="date"
@@ -69,7 +69,7 @@ const NewProjectForm = () => {
               required
             />
           </div>
-          <div>
+          <div style={{paddingTop: '10px'}}>
             <h2>End Date:</h2>
             <input
               type="date"
@@ -77,6 +77,17 @@ const NewProjectForm = () => {
               onChange={(e) => setEndDate(e.target.value)}
               required
             />
+          </div>
+          <div style={{paddingTop: '10px'}}>
+
+            <h2>Add Contributor</h2>
+            <input
+              type="text"
+              placeholder='Email Address'
+              style={{ marginLeft: '10px', marginTop: '25px' }}
+              
+            />
+            <button type="button" style={{ backgroundColor: '#F4F1E7', color: 'black', padding: '5px 10px', cursor: 'pointer', borderRadius: '0'}}>Add Contributor</button>
           </div>
           <div style={{ textAlign: 'center', marginTop: '20px' }}>
           <button type="submit" style={{ backgroundColor: '#1BA098', color: 'black', padding: '5px 10px', cursor: 'pointer', borderRadius: '0' }}>Create Project</button>
