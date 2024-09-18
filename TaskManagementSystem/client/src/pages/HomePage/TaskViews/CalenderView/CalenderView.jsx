eed4d52fb2640973ba7b0601f97e5f2938007977
John Bui <jbui0004@student.monash.edu>
2024-09-18 18:24:37 +1000

    Add calendar view

--- TaskManagementSystem/client/src/pages/HomePage/TaskViews/CalenderView/CalenderView.jsx
+++ TaskManagementSystem/client/src/pages/HomePage/TaskViews/CalenderView/CalenderView.jsx
@@ -1,1 +1,41 @@
-//Bad practice to create this file before we need it btw but im doing it anyway to give us a structure
+import React, { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
+
+const CalendarView = () => {
+  const [events, setEvents] = useState([
+    {
+      title: 'Task 1',
+      start: new Date(),
+      end: new Date(),
+      allDay: false,
+    },
+    {
+      title: 'Task 2',
+      start: new Date(2024, 8, 20, 14, 0),
+      end: new Date(2024, 8, 20, 16, 0),
+      allDay: false,
+    },
+  ]);
+
+  return (
+    <div style={{display: 'flex', flexDirection: 'column', height: '90vh'}}>
+      <h1 style={{ textAlign: 'left', padding: '40px 0 0 0' }}>Calendar View</h1>
+      <hr /><br/>
+      <div style={{flexGrow: '1', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+        <Calendar
+          localizer={localizer}
+          events={events}
+          startAccessor="start"
+          endAccessor="end"
+          style={{ height: '100%', width: '100%' }}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default CalendarView;
