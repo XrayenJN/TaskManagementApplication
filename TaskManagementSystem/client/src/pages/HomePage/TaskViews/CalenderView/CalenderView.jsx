27f6696e2cc866cf305f6afccb760fd8edcbc0fe
John Bui <jbui0004@student.monash.edu>
2024-09-19 13:15:36 +1000

    Add add task button & Remove agenda tab

--- TaskManagementSystem/client/src/pages/HomePage/TaskViews/CalenderView/CalenderView.jsx
+++ TaskManagementSystem/client/src/pages/HomePage/TaskViews/CalenderView/CalenderView.jsx
@@ -44,6 +44,9 @@ const CalendarView = () => {
   return (
     <div style={{display: 'flex', flexDirection: 'column', height: '90vh'}}>
       <h1 style={{ textAlign: 'left', padding: '40px 0 0 0' }}>Calendar View</h1>
+      <Link to={`/project/${projectId}/new-project-task-form`} className="kanban-button" style={{ display: 'flex', width: '70px', position: 'absolute', margin: '90px', right: '-60px' }}>
+        Add task
+      </Link>
       <hr style={{padding: '0 1500px 0 0', marginTop: '-20px'}}/><br/>
       <div style={{flexGrow: '1', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <Calendar
@@ -51,6 +54,7 @@ const CalendarView = () => {
           events={events}
           startAccessor="start"
           endAccessor="end"
+          views={['month', 'week', 'day']}
           style={{ height: '100%', width: '100%' }}
           eventPropGetter={eventStyleGetter}
           onSelectEvent={(event) => alert(`Task: ${event.title}\nDescription: ${event.description}\nStatus: ${event.status}\nOwners: ${event.owners.join(', ')}`)}
