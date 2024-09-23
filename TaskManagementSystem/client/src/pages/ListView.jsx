2bac4147f1f644297fc17f476203a15c1b6a10d0
Edward <wodainimenda1@gmail.com>
2024-09-23 14:35:21 +1000

    Formatted the task view from close due date to late due date

--- TaskManagementSystem/client/src/pages/ListView.jsx
+++ TaskManagementSystem/client/src/pages/ListView.jsx
@@ -75,9 +75,10 @@ const ListView = () => {
 
   const tasksOutput = () => {
     if (groupedTasks) {
+      const sortedTaskByDates = Object.keys(groupedTasks).sort((a, b) => new Date(a) - new Date(b));
       return (
         <ul>
-          {Object.keys(groupedTasks).map(date => (
+          {sortedTaskByDates.map(date => (
             <li key={date} style={{ marginBottom: '30px' }}>
               <div style={{ textAlign: 'left', marginBottom: '10px', color: 'black', fontSize: '20px', fontWeight: 'bold' }}>
                 <i>{date}</i>
@@ -96,7 +97,142 @@ const ListView = () => {
                   <button onClick={() => togglePopup(task)} style={{ backgroundColor: '#DEB992', color: 'black', padding: '10px 20px', border: 'none', cursor: 'pointer' }}>
                     Edit Project Details
                   </button>
+                  {showPopup && (
+                <div className="popup">
+                  <div className="popup-content" style={{ backgroundColor: '#DEB992' }}>
+                    <h2>Edit Task Details</h2>
+                    <hr />
+
+                    <div>
+                      <table style={{ margin: 'auto' }}>
+                        <tbody>
+                          <tr>
+                            <td>
+                              <div>
+                                <h3><u>Task Name</u></h3>
+                                <input
+                                  type="text"
+                                  name="name"
+                                  value={editedTask.name}
+                                  onChange={handleInputChange}
+                                />
+                              </div>
+
+                              <div>
+                                <h3><u>Task Description</u></h3>
+                                <textarea
+                                  name="description"
+                                  style={{ color: 'black' }}
+                                  value={editedTask.description}
+                                  onChange={handleInputChange}
+                                />
+                              </div>
+                              <div>
+                                <h3><u>Task Comments</u></h3>
+                                <textarea
+                                  type="text"
+                                  name="comments"
+                                  value={editedTask.comments}
+                                  onChange={handleInputChange}
+                                />
+                              </div>
+                            </td>
+                            <td>
+                              <div>
+                                <h3><u>Task Links</u></h3>
+                                <textarea
+                                  name="links"
+                                  style={{ color: 'black' }}
+                                  value={editedTask.links}
+                                  onChange={handleInputChange}
+                                />
+                              </div>
+                              <div>
+                              <label>
+                                <input
+                                  name="isMilestone"
+                                  type="checkbox"
+                                  checked={editedTask.isMilestone}
+                                  onChange={handleInputChange}
+                                />
+                                  Milestone
+                                </label>
+                              </div>
+
+                              <div>
+                                <h3><u>Task Status</u></h3>
+                                <select
+                                  name="status"
+                                  value={editedTask.status}
+                                  onChange={handleInputChange}
+                                  required
+                                >
+                                  <option value="">Select Status</option>
+                                  <option value="Backlog">Backlog</option>
+                                  <option value="Ready">Ready</option>
+                                  <option value="InProgress">InProgress</option>
+                                  <option value="Completed">Completed</option>
+                                </select>
+                              </div>
+                              <div>
+                                <select
+                                  name="owners"
+                                  value={editedTask.owners}
+                                  onChange={handleInputChange}
+                                  required
+                                >
+                                  <option value="">Select Owner</option>
+                                  {contributors.map((contributor, index) => (
+                                    <option key={index} value={contributor.email}>
+                                      {contributor.name}
+                                    </option>
+                                  ))}
+                                </select>
+                              </div>
+                              <div>
+                                <table style={{ margin: 'auto' }}>
+                                  <thead>
+                                    <tr>
+                                      <th><h3><u>Start Date</u></h3></th>
+                                      <th><h3><u>End Date</u></h3></th>
+                                    </tr>
+                                  </thead>
+                                  <tbody>
+                                    <tr>
+                                      <td>
+                                        <input
+                                          type="date"
+                                          name="startDate"
+                                          value={editedTask.startDate}
+                                          onChange={handleInputChange}
+                                        />
+                                      </td>
+                                      <td>
+                                        <input
+                                          type="date"
+                                          name="endDate"
+                                          value={editedTask.endDate}
+                                          onChange={handleInputChange}
+                                        />
+                                      </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </div>
+                            </td>
+                          </tr>
+                        </tbody>
+                      </table>
+                    </div>
+                    <hr />
+                    <div>
+                      <button onClick={handleSave}>Save</button>
+                      <button onClick={() => setShowPopup(false)}>Close</button>
+                    </div>
+                  </div>
                 </div>
+                )}
+              </div>
               ))}
             </li>
           ))}
@@ -108,7 +244,7 @@ const ListView = () => {
   return (
     <div>
       <div>
-        <h1>Project Task List</h1>
+        <h1 style={{ textAlign: 'left', marginTop: '50px' }}>Project Task List</h1>
         <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
           <button style={{ backgroundColor: '#DEB992', color: 'black', padding: '10px 20px', border: 'none', cursor: 'pointer' }}>Filter</button>
           <button style={{ backgroundColor: '#DEB992', color: 'black', padding: '10px 20px', border: 'none', cursor: 'pointer' }}>Sort By</button>
