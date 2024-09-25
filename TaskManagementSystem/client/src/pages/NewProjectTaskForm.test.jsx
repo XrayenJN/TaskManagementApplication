abd040399c6a39651ab411edc9f9bbd057370eae
Ethan Fernandez <ethanjuf@gmail.com>
2024-09-25 16:09:48 +1000

    - Trying to mock firebase to be able to test NewProjectTaskForm

--- TaskManagementSystem/client/src/pages/NewProjectTaskForm.test.jsx
+++ TaskManagementSystem/client/src/pages/NewProjectTaskForm.test.jsx
@@ -1,22 +1,38 @@
 import { render, fireEvent } from '@testing-library/react';
 import NewProjectTaskForm from './NewProjectTaskForm.jsx';
 import { expect, jest, test } from '@jest/globals';
+import { createNewProjectTaskDocument, getContributors } from '../firebase/firebase';
 
-jest.mock('../firebase/firebase', () => {
-    createNewProjectTaskDocument: jest.fn()
-    getContributors: jest.fn()
+const mockedDatabaseData = [{
+    "name": "Ethan Fernandez",
+    "email": "efer0018@student.monash.edu"
+}]
+jest.spyOn(NewProjectTaskForm, getContributors).mockImplementation(() => {
+    return mockedDatabaseData
+})
+jest.spyOn(NewProjectTaskForm, createNewProjectTaskDocument).mockImplementation(() => {
+    return null
 })
 
+jest.spyOn(NewProjectTaskForm, retrieveContributors).mockImplementation(() => {
+    return mockedDatabaseData
+})
+jest.spyOn(NewProjectTaskForm, app).mockImplementation(() => {
+    return null
+})
+
+
+// jest.mock('../firebase/firebase', () => {
+//     createNewProjectTaskDocument: jest.fn(() => {
+//         return null
+//     })
+//     getContributors: jest.fn(() => {
+//         return null
+//     })
+// })
+
 it('renders a textarea with placeholder and calls setComments on change', () => {
-    const getByPlaceholderText = render(NewProjectTaskForm);
-    const mockedDatabaseData = [{
-        "name": "Ethan Fernandez",
-        "email": "efer0018@student.monash.edu"
-    }]
-    jest.spyOn(NewProjectTaskForm, getContributors).mockImplementation(() => {
-        return mockedDatabaseData
-    })
-    jest.spyOn(NewProjectTaskForm, createNewProjectTaskDocument).mockImplementation(() => {
-        return null
-    })
+    
+    // const getByPlaceholderText = render(NewProjectTaskForm);
+    test
 });
